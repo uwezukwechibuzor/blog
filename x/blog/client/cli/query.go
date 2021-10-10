4aab796e4f04dc88fd7117a4f7e29cdcf97eae1c
uwezukwechibuzor <uwezukwechibuzor@gmail.com>
2021-10-10 23:59:18 +0000

    adding queryComment cli

--- x/blog/client/cli/query.go
+++ x/blog/client/cli/query.go
@@ -27,7 +27,9 @@ func GetQueryCmd(queryRoute string) *cobra.Command {
 	// this line is used by starport scaffolding # 1
 	cmd.AddCommand(CmdListPost())
 	cmd.AddCommand(CmdShowPost())
-
+    
+	cmd.AddCommand(CmdListComment())
+	cmd.AddCommand(CmdShowComment())
   
 	return cmd
 }
